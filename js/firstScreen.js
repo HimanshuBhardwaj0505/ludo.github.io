c757d942746acc9bda11d467d89e32ff4732632b
HimanshuBhardwaj0505 <bhardwajHimanshu0505@gmail.com>
2021-11-27 16:56:04 +0530

    Ludo Game

---
+++ js/firstScreen.js
@@ -0,0 +1,20 @@
+'use strict'
+
+setTimeout(() => {
+
+    var splash = document.getElementById('splash');
+    splash.style.opacity = `0`;
+
+    var loader = document.getElementById('loader');
+    loader.style.opacity = `0`;
+
+
+}, 3000);
+
+
+setTimeout(() => {
+
+    var card = document.getElementById('card');
+    card.style.opacity = `5`;
+
+}, 4000);
